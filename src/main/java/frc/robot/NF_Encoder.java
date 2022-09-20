2a467f2f92a360283fabd1607f914bb954e7616a
yehpop <smsglxyn8@gmail.com>
2022-09-20 21:08:02 +0300

    nfencoder

---
+++ src/main/java/frc/robot/NF_Encoder.java
@@ -0,0 +1,22 @@
+/* i do not know java */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+/*
+ * Class to read data from Encoder
+ * specifically from the encoder programmed by team nf for frc off season 2022
+ * 
+ *
+ *
+ *
+ *
+ *
+ * 
+ */
+public class NF_Encoder extends Encoder {
+
+
+
+};
